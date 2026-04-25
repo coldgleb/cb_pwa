fc529e592fa68e33c987973378f79c5e774c3d27
coldgleb <saygingleb101@gmail.com>
2026-04-26 01:28:43 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
+// Next.js обычно использует .env.local, подгружаем его тоже
+dotenv.config({ path: ".env.local" });
 dotenv.config();
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   },
 });
